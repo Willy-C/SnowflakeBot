f1677091edb742cdd084e2b8830ec93a376cfaa3
Willy <willychan@live.ca>
2019-06-10 11:39:39 -0400

    Now checks for recency of message and activity of target

--- cogs/highlight.py
+++ cogs/highlight.py
@@ -12,16 +12,21 @@ class HighlightCog(commands.Cog, name='Highlight'):
         self.highlights = {'willy': 94271181271605248}
         self.keys = self.highlights.keys()
 
-    async def _get_msg_context(self, message: discord.Message):
-        prev_msg = await message.channel.history(limit=5, before=message).flatten()
+    async def _get_msg_context(self, message: discord.Message, key: str):
+        prev_msg = await message.channel.history(limit=5, before=message).flatten()  # Grabs the previous 5 messages
         formatted = []
         dateformat = '%m-%d %H:%M:%S'
         for msg in prev_msg:
-            formatted.append(
-                f'[{msg.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).strftime(dateformat)}] {msg.content}')
+            if (
+                    datetime.utcnow() - msg.created_at).total_seconds() < 900:  # Only care about messages within last 15 minutes
+                if msg.author.id == self.highlights[key]:  # If target recently spoke, no DM
+                    return
+                formatted.append(
+                    f'[{msg.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).strftime(dateformat)}] {msg.author}: {msg.content}')
+
         formatted.reverse()
         formatted.append(
-            f'[{message.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).strftime(dateformat)}] {message.content}')
+            f'[{message.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).strftime(dateformat)}] {message.author}: {message.content}')
         return '\n'.join(formatted)
 
     async def _dm_highlight(self, message: discord.Message, key: str):
@@ -29,11 +34,19 @@ class HighlightCog(commands.Cog, name='Highlight'):
         target = self.bot.get_user(self.highlights[key])
         if message.author == target:
             return
-        context = await self._get_msg_context(message)
-        # await target.send(f'You were mentioned in {discord.utils.escape_markdown(message.guild.name)} | {discord.utils.escape_markdown(message.channel.name)}\n'
-        await target.send(f'You were mentioned in {message.guild.name} | {message.channel}\n'
-                          f'{context}\n'
-                          f'Jump link: {message.jump_url}')
+        context = await self._get_msg_context(message, key)
+
+        if context is None:  # target recently messaged, no need to DM
+            return
+
+        e = discord.Embed(title=f'You were mentioned in {message.guild.name} | #{message.channel}',
+                          description=f'{context}\n'
+                                      f'[Jump to message]({message.jump_url})',
+                          color=discord.Color.blue())
+        await target.send(embed=e)
+        # await target.send(f'You were mentioned in {message.guild.name} | {message.channel}\n'
+        #                   f'{context}\n'
+        #                   f'Jump link: {message.jump_url}')
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -44,6 +57,9 @@ class HighlightCog(commands.Cog, name='Highlight'):
             if key.lower() in message.content.lower():
                 await self._dm_highlight(message, key)
 
+
+# TODO: Ignore if user is in conversation - check if user recently spoke
+# TODO: Only add to context if message is recent
 
 def setup(bot):
     bot.add_cog(HighlightCog(bot))
