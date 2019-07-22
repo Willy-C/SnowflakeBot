21ee755cd6266fae1513cb38cbd8d36d4a8eb149
Willy Chan <willyc.chan@mail.utoronto.ca>
2019-07-22 21:04:02 +0800

    Added function that outputs vibrant discord.Color's

--- global_utils.py
+++ global_utils.py
@@ -3,6 +3,9 @@ import copy
 import discord
 from discord.ext import commands
 
+import colorsys
+import random
+
 
 async def copy_context(ctx: commands.Context, *, author=None, channel=None, **kwargs):
     """
@@ -17,3 +20,12 @@ async def copy_context(ctx: commands.Context, *, author=None, channel=None, **kw
 
     # obtain and return a context of the same type
     return await ctx.bot.get_context(alt_message, cls=type(ctx))
+
+
+def bright_color():
+    """
+    Returns a random discord.Color that does not look ugly or dull
+    """
+    values = [int(x*255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1)]
+    color = discord.Color.from_rgb(*values)
+    return color
