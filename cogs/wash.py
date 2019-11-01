18671a036a72c9a5880c50e4ec9a27bca2cdec5b
Willy <willychan@live.ca>
2019-10-31 20:53:00 -0400

    Added setalert command and other refactoring

--- cogs/wash.py
+++ cogs/wash.py
@@ -13,22 +13,33 @@ class WASHCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.last_msg = datetime.utcnow()
+        self._timeout = 3600
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.guild is None or message.guild.id != GUILD_ID or message.channel.id != CHANNEL_ID or message.author.bot:
+        if message.channel.id != CHANNEL_ID or message.author.bot:
             return
 
-        if (message.created_at - self.last_msg).seconds > 5400:
-            await self.send_alert(message.channel)
+        if (message.created_at - self.last_msg).seconds > self._timeout:
+            await self.send_alert(message)
         self.last_msg = message.created_at
 
-    async def send_alert(self, target):
-        await target.send(f'<@{HAD_ID}> A conversation just started!')
+    async def send_alert(self, message):
+        if message.author.id == HAD_ID:
+            return
+        await message.channel.send(f'<@{HAD_ID}> A conversation just started!')
 
     # If a new conversation started, then ping
-    # new conversation = new message where the previous message is over 2 hours old
+    # new conversation = new message where the previous message is over `self._timeout` seconds old
 
+    @commands.command(name='setalert', hidden=True)
+    async def set_timeout(self, ctx, time: int):
+        if ctx.guild and ctx.guild.id != GUILD_ID:
+            return
+        if time < 0:
+            return await ctx.send(f'Please enter a positive number.')
+        self._timeout = time
+        await ctx.send(f'I will now send a ping when there is a new message after {time}s of silence')
 
 def setup(bot):
     bot.add_cog(WASHCog(bot))
