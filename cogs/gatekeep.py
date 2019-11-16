a17467d3413f30d5988ca572023316348f65b6e7
Willy <willychan@live.ca>
2019-11-16 14:12:28 -0500

    Added verify command

--- cogs/gatekeep.py
+++ cogs/gatekeep.py
@@ -25,17 +25,25 @@ class GatekeepCog(commands.Cog):
         if member.id in self.verified:
             role = member.guild.get_role(VERIFIED_ROLE)
             try:
-                await member.add_roles(role)
+                await member.add_roles(role, reason='Automatic verification')
             except (discord.HTTPException, AttributeError) as err:
                 await join_channel.send(f'Unable to verify {member}\n```{err}```')
 
             verified = '<:greenTick:602811779835494410>'
             color = 0x55dd55
+            title ='Verified Member Join'
+
+        elif member.bot:
+            verified = '<:greenTick:602811779835494410>'
+            color = 0x55dd55
+            title ='Bot Join'
+
         else:
             verified = '<:redTick:602811779474522113>'
             color = 0xdda453
+            title = 'Member Join'
 
-        e = discord.Embed(title='Member Join',
+        e = discord.Embed(title=title,
                           color=color,
                           timestamp=datetime.utcnow())
         e.set_author(icon_url=member.avatar_url, name=member)
@@ -51,7 +59,7 @@ class GatekeepCog(commands.Cog):
         if member.guild.id != GUILD_ID:
             return
 
-        e = discord.Embed(title=f'Member leave',
+        e = discord.Embed(title='Member leave',
                           color=discord.Color(0xff0000),
                           timestamp=datetime.utcnow())
         e.set_author(icon_url=member.avatar_url, name=member)
@@ -66,6 +74,29 @@ class GatekeepCog(commands.Cog):
         e.add_field(name='Created', value=f'{humanize.naturaldelta((datetime.utcnow() - member.created_at))} ago')
 
         await self.bot.get_channel(JOIN_CHANNEL).send(embed=e)
+
+    @commands.command(hidden=True)
+    async def verify(self, ctx, member: discord.Member):
+        if ctx.guild is None or ctx.guild.id != GUILD_ID:
+            return
+        if ctx.author.id not in self.verified:
+            await ctx.send('Sorry, you do not have permission to do that!')
+            return
+        role = discord.Object(id=VERIFIED_ROLE)
+        await member.add_roles(role, reason=f'Manual Verification by {ctx.author}')
+
+    @verify.error()
+    async def verify_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            ctx.local_handled = True
+            return await ctx.send('```No user found on this server matching that name.\n'
+                                  'I will search in this order: \n'
+                                  '1. By ID                     (ex. 5429519026699)\n'
+                                  '2. By Mention                (ex. @Snowflake)\n'
+                                  '3. By Name#Discrim           (ex. Snowflake#7321)\n'
+                                  '4. By Name                   (ex. Snowflake)\n'
+                                  '5. By Nickname               (ex. BeepBoop)\n'
+                                  'Note: Names are Case-sensitive!```')
 
 
 def setup(bot):
