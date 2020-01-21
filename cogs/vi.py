f95256121f8e011475febeba8bb784600279ab5c
Willy <willychan@live.ca>
2020-01-20 23:19:04 -0500

    checks if user is in the channel to use command

--- cogs/vi.py
+++ cogs/vi.py
@@ -11,7 +11,7 @@ FLOATER_ROLE_ID = 567539820545572865
 class ViCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.vc_history = {597288448562429972: {'join': None,
+        self.vc_history = {658169579452760099: {'join': None,
                                                 'leave': None},
                            631344051442155520: {'join': None,
                                                 'leave': None}}
@@ -42,23 +42,26 @@ class ViCog(commands.Cog):
     async def who(self, ctx, *, voicechannel: discord.VoiceChannel = None):
         if ctx.guild and ctx.guild.id != GUILD_ID:
             return
-        if voicechannel is None:
-            if ctx.author.voice is None:
-                return await ctx.send('Please specify a voice channel')
-            voicechannel = ctx.author.voice.channel
+        if ctx.author.voice is not None:
+            voicechannel = voicechannel or ctx.author.voice.channel
+        if ctx.author.id not in (ctx.guild.owner_id, self.bot.owner_id) and (ctx.author.voice is None or ctx.author.voice.channel != voicechannel):
+            return await ctx.send('You are not in that voice channel!', delete_after=15)
+
+        if voicechannel is None and ctx.author.voice is None:
+            return await ctx.send('Please specify a voice channel', delete_after=15)
 
         if voicechannel.id not in self.vc_history:
-            return await ctx.send('Sorry, I am not monitoring this vc\'s history', delete_after=30)
+            return await ctx.send('Sorry, I am not monitoring this vc\'s history', delete_after=15)
 
         if self.vc_history[voicechannel.id]['leave'] is not None:
             last_leave, leave_time = self.vc_history[voicechannel.id]['leave']
             leave_delta = humanize.naturaldelta(datetime.utcnow() - leave_time)
-            await ctx.send(f'Last person to leave {voicechannel} was {last_leave} - {leave_delta} ago')
+            await ctx.send(f'Last person to leave {voicechannel} was {last_leave} - {leave_delta} ago', delete_after=30)
 
         if self.vc_history[voicechannel.id]['join'] is not None:
             last_join, join_time = self.vc_history[voicechannel.id]['join']
             join_delta = humanize.naturaldelta(datetime.utcnow() - join_time)
-            await ctx.send(f'Last person to join {voicechannel} was {last_join} - {join_delta} ago')
+            await ctx.send(f'Last person to join {voicechannel} was {last_join} - {join_delta} ago', delete_after=30)
 
 
 def setup(bot):
