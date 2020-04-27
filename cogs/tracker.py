7f3d5e85fb0c1b48c0ee1b204f414c72b5fccb1a
Willy <willychan@live.ca>
2020-04-27 09:50:08 -0400

    Refactor

--- cogs/tracker.py
+++ cogs/tracker.py
@@ -44,6 +44,9 @@ class TrackerCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
+        existing_ava = '''SELECT DISTINCT id FROM avatar_changes;'''
+        records = await self.bot.pool.fetch(existing_ava)
+        ava_ids = {record['id'] for record in records}
         for member in guild.members:
             query = '''INSERT INTO first_join(guild, "user", time)
                    VALUES($1, $2, $3);'''
@@ -52,42 +55,55 @@ class TrackerCog(commands.Cog):
                 await self.bot.pool.execute(query, member.guild.id, member.id, join_time)
             except Exception:
                 traceback.print_exc()
-
-    @commands.Cog.listener()
-    async def on_user_update(self, before: discord.User, after: discord.User):
-        if before.name != after.name or before.discriminator != after.discriminator:
-            query = '''INSERT INTO name_changes(id, name, discrim, changed_at)
-                       VALUES($1, $2, $3, $4);'''
-            await self.bot.pool.execute(query, after.id, after.name, after.discriminator, datetime.utcnow())
-        if before.avatar != after.avatar:
-            if after.avatar:
-                hash = after.avatar
-                type = 'gif' if hash.startswith('a_') else 'png'
-                file = discord.File(BytesIO(await after.avatar_url_as(static_format='png').read()),
-                                    filename=f'{hash}.{type}')
-                wh = discord.utils.get(await self.bot.get_guild(557306479191916555).webhooks(),
-                                       channel_id=703171905435467956)
-                if wh is not None:
-                    msg = await wh.send(content=hash, file=file, wait=True, username=after.id)
-                else:
-                    self.bot.get_channel(703171905435467956)
-                    msg = await self.bot.get_channel(703171905435467956).send(content=hash, file=file)
-                url = msg.attachments[0].url
-                message_id = msg.id
-            else:
-                hash = after.default_avatar.name
-                url = after.default_avatar_url
-                message_id = None
-            query = '''INSERT INTO avatar_changes(id, hash, url, message, changed_at)
-                       VALUES($1, $2, $3, $4, $5);'''
-            await self.bot.pool.execute(query, after.id, hash, url, message_id, datetime.utcnow())
+            if member.nick:
+                await self.log_nickname(member)
+            if member.id not in ava_ids:
+                await self.log_avatar(member)
 
     @commands.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member):
         if before.nick != after.nick and after.nick is not None:
-            query = '''INSERT INTO nick_changes(id, guild, name, changed_at)
-                       VALUES($1, $2, $3, $4);'''
-            await self.bot.pool.execute(query, after.id, after.guild.id, after.nick, datetime.utcnow())
+            await self.log_nickname(after)
+
+    async def log_nickname(self, member: discord.Member):
+        query = '''INSERT INTO nick_changes(id, guild, name, changed_at)
+                   VALUES($1, $2, $3, $4);'''
+        await self.bot.pool.execute(query, member.id, member.guild.id, member.nick, datetime.utcnow())
+
+    @commands.Cog.listener()
+    async def on_user_update(self, before: discord.User, after: discord.User):
+        if before.name != after.name or before.discriminator != after.discriminator:
+            await self.log_username(after)
+        if before.avatar != after.avatar:
+            await self.log_avatar(after)
+
+    async def log_username(self, user: discord.User):
+        query = '''INSERT INTO name_changes(id, name, discrim, changed_at)
+                   VALUES($1, $2, $3, $4);'''
+        await self.bot.pool.execute(query, user.id, user.name, user.discriminator, datetime.utcnow())
+
+    async def log_avatar(self, user: discord.User):
+        if user.avatar:
+            hash = user.avatar
+            type = 'gif' if hash.startswith('a_') else 'png'
+            file = discord.File(BytesIO(await user.avatar_url_as(static_format='png').read()),
+                                filename=f'{hash}.{type}')
+            wh = discord.utils.get(await self.bot.get_guild(557306479191916555).webhooks(),
+                                   channel_id=703171905435467956)
+            if wh is not None:
+                msg = await wh.send(content=hash, file=file, wait=True, username=user.id)
+            else:
+                self.bot.get_channel(703171905435467956)
+                msg = await self.bot.get_channel(703171905435467956).send(content=hash, file=file)
+            url = msg.attachments[0].url
+            message_id = msg.id
+        else:
+            hash = user.default_avatar.name
+            url = str(user.default_avatar_url)
+            message_id = None
+        query = '''INSERT INTO avatar_changes(id, hash, url, message, changed_at)
+                   VALUES($1, $2, $3, $4, $5);'''
+        await self.bot.pool.execute(query, user.id, hash, url, message_id, datetime.utcnow())
 
 
 def setup(bot):
