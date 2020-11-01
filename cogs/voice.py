bf9cc21cdcb8bec47a0840cc681256e89834dd8d
Willy <willychan@live.ca>
2020-11-01 01:17:42 -0400

    Upgrade author to Member when used in DM

--- cogs/voice.py
+++ cogs/voice.py
@@ -15,20 +15,29 @@ class Voice(commands.Cog):
         voicechannel = voicechannel or ctx.author.voice.channel
         if not voicechannel.guild.me.guild_permissions.mute_members:
             return await ctx.send('I do not have permission to mute!')
+        if not ctx.guild:
+            ctx.author = voicechannel.guild.get_member(ctx.author.id)
 
         await ctx.message.add_reaction('<a:typing:559157048919457801>')
+
+        current_members = voicechannel.members
 
         def check(member, before, after):
             return (member != ctx.author
                     and before.channel != voicechannel
                     and after.channel == voicechannel
-                    and ctx.author in voicechannel.members)
+                    and ctx.author in voicechannel.members
+                    and member not in current_members) \
+                   or (member == ctx.author
+                       and before.channel == voicechannel
+                       and after.channel != voicechannel)
         try:
-            await self.bot.wait_for('voice_state_update', check=check, timeout=43200)
+            member, _ , _ = await self.bot.wait_for('voice_state_update', check=check, timeout=43200)
         except TimeoutError:
             pass
         else:
-            await ctx.author.edit(mute=True)
+            if member != ctx.author:
+                await ctx.author.edit(mute=True)
         await ctx.message.add_reaction('<:greenTick:602811779835494410>')
         try:
             await ctx.message.remove_reaction('<a:typing:559157048919457801>', ctx.me)
