b81b824d7af395bfb7e393fc80c9d08f47951efa
Willy <willychan@live.ca>
2020-11-09 20:05:39 -0500

    Reaction to unmute

--- cogs/voice.py
+++ cogs/voice.py
@@ -27,6 +27,7 @@ class Voice(commands.Cog):
 
         def check(member, before, after):
             return (member != ctx.author
+                    and not member.bot
                     and before.channel != voicechannel
                     and after.channel == voicechannel
                     and ctx.author in voicechannel.members
@@ -38,6 +39,7 @@ class Voice(commands.Cog):
             member, _, _ = await self.bot.wait_for('voice_state_update', check=check, timeout=43200)
         except TimeoutError:
             await ctx.message.add_reaction('<:status_idle:602811780129095701>')
+            return
         else:
             if member != ctx.author:
                 await ctx.author.edit(mute=True)
@@ -49,10 +51,22 @@ class Voice(commands.Cog):
                 await ctx.message.remove_reaction('<a:typing:559157048919457801>', ctx.me)
                 await ctx.message.remove_reaction('<:status_online:602811779948740627>', ctx.me)
 
+        if member != ctx.author:
+            await ctx.message.add_reaction('\U0000203c')
 
-
-
-
+            def reaction_check(reaction, user):
+                return reaction.message.id == ctx.message.id and user == ctx.author and str(reaction.emoji) == '\U0000203c'
+            try:
+                await self.bot.wait_for('reaction_add', check=reaction_check, timeout=120)
+            except TimeoutError:
+                pass
+            else:
+                await ctx.author.edit(mute=False)
+            finally:
+                try:
+                    await ctx.message.clear_reaction('\U0000203c')
+                except discord.HTTPException:
+                    pass
 
 
 def setup(bot):
