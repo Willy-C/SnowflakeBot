b116cd9eb02dd253f5f8c71ecd5e2f342a905d33
Willy <19799671+Willy-C@users.noreply.github.com>
2021-08-17 14:37:22 -0400

    Suppress CommandErrors raised when doing can_run checks and added documentation
    Signed-off-by: Willy <19799671+Willy-C@users.noreply.github.com>

--- cogs/fuzzy.py
+++ cogs/fuzzy.py
@@ -2,28 +2,37 @@ import discord
 import rapidfuzz
 from discord.ext import commands
 from utils.global_utils import copy_context
+from typing import List, Tuple, Optional
 
 
 class Levenshtein(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_all_runnable_commands(self, ctx: commands.Context, include_aliases=False):
+    async def get_all_runnable_commands(self, ctx: commands.Context, include_aliases=False) -> List[str]:
+        """Returns a list of command names that the user can run. This does not include aliases and subcommands."""
         all_command_names = []
         is_owner = await self.bot.is_owner(ctx.author)
         for command in self.bot.commands:
-            command: commands.Command
-            if await command.can_run(ctx):
-                if not is_owner and command.hidden:
-                    continue
-                all_command_names.append(command.name)
-                if include_aliases:
-                    for alias in command.aliases:
-                        all_command_names.append(alias)
+            try:
+                can_run = await command.can_run(ctx)
+            except commands.CommandError:
+                continue
+            else:
+                if can_run:
+                    if not is_owner and command.hidden:
+                        continue
+                    all_command_names.append(command.name)
+                    if include_aliases:
+                        for alias in command.aliases:
+                            all_command_names.append(alias)
 
         return all_command_names
 
-    async def correct_command_name(self, ctx: commands.Context):
+    async def correct_command_name(self, ctx: commands.Context) -> Optional[Tuple[str, int, int]]:
+        """Returns an Optional[Tuple] for closest command name matched within 1 levenshtein distance.
+        Returns (command name, similarity score (this is always 1), index of command in list of commands)
+        Returns None if no match found within 1 levenshtein distance"""
         runnable_commands = await self.get_all_runnable_commands(ctx, include_aliases=False)
         if not runnable_commands:
             return
