9526ca6aacfcb675e7deb3a402a044cd35e4666b
Willy <19799671+Willy-C@users.noreply.github.com>
2021-08-25 18:11:40 -0400

    Update confirm prompt to use Buttons

--- utils/context.py
+++ utils/context.py
@@ -1,10 +1,49 @@
-import discord
-from discord.ext import commands
-
 import io
 import traceback
 import contextlib
-from asyncio import TimeoutError
+from typing import Optional
+
+import discord
+from discord.ext import commands
+
+
+class ConfirmView(discord.ui.View):
+    def __init__(self, *, context: Context, timeout: float, author: discord.User, delete_after: bool):
+        super().__init__(timeout=timeout)
+        self.ctx: Context = context
+        self.author: discord.User = author
+        self.delete_after: bool = delete_after
+        self.message: Optional[discord.Message] = None
+        self.choice: Optional[bool] = None
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user is None:
+            return False
+        if interaction.user == self.author:
+            return True
+        else:
+            await interaction.response.send_message('You cannot use this', ephemeral=True)
+            return False
+
+    async def on_timeout(self) -> None:
+        if self.message and self.delete_after:
+            await self.message.delete()
+
+    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
+    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.choice = True
+        await interaction.response.defer()
+        if self.delete_after:
+            await interaction.delete_original_message()
+        self.stop()
+
+    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.red)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.choice = False
+        await interaction.response.defer()
+        if self.delete_after:
+            await interaction.delete_original_message()
+        self.stop()
 
 
 class Context(commands.Context):
@@ -22,72 +61,19 @@ class Context(commands.Context):
 
     @discord.utils.cached_property
     def replied_reference(self):
-        ref = self.message.reference
-        if ref and isinstance(ref.resolved, discord.Message):
-            return ref.resolved.to_reference()
+        if message := self.replied_message:
+            return message.to_reference()
         return None
 
-    async def confirm_prompt(self, msg, extra=True):
+    async def confirm_prompt(self, msg, *, timeout=60, delete_after=True):
         """
-        Asks author for confirmation, returns True if confirmed, False if user typed abort or timed out
+        Asks author for confirmation
+        Returns True if confirmed, False if cancelled, None if timed out
         """
-        def confirm(m):
-            if self.author.id != m.author.id or self.channel.id != m.channel.id:
-                return False
-            return m.content.lower() in ('**confirm**', 'confirm', '**abort**', 'abort')
-
-        if extra:
-            msg += '\nPlease type **confirm** within 1 minute to continue or type **abort** if you change your mind.'
-
-        prompt = await self.reply(msg)
-
-        try:
-            reply = await self.bot.wait_for('message', check=confirm, timeout=60)
-        except TimeoutError:
-            await self.send('1 minute has passed. Aborting...', delete_after=5)
-            return False
-        else:
-            choice = reply.content
-            await reply.delete()
-            if choice.lower() in ('**confirm**', 'confirm'):
-                return True
-            else:
-                await self.send('Aborting...', delete_after=5)
-                return False
-        finally:
-            with contextlib.suppress(discord.HTTPException):
-                await prompt.delete()
-
-    async def confirm_reaction(self, msg, extra=True):
-        """
-        Asks author for confirmation via reactions, returns True if confirmed, False if user clicked X or timed out
-        """
-        emojis = ['<:greenTick:602811779835494410>', '<:redTick:602811779474522113>']
-
-        def confirm(r, u):
-            return self.author.id == u.id and prompt == r.message and str(r.emoji) in emojis
-
-        if extra:
-            msg += '\nPlease react with <:greenTick:602811779835494410> within 1 minute to continue or <:redTick:602811779474522113> if you change your mind.'
-
-        prompt = await self.reply(msg)
-
-        for e in emojis:
-            await prompt.add_reaction(e)
-        try:
-            reaction, _ = await self.bot.wait_for('reaction_add', check=confirm, timeout=60)
-        except TimeoutError:
-            await self.send('1 minute has passed. Aborting...', delete_after=5)
-            return False
-        else:
-            if str(reaction.emoji) == '<:greenTick:602811779835494410>':
-                return True
-            else:
-                await self.send('Aborting...', delete_after=5)
-                return False
-        finally:
-            with contextlib.suppress(discord.HTTPException):
-                await prompt.delete()
+        view = ConfirmView(context=self, timeout=timeout, author=self.author, delete_after=delete_after)
+        view.message = await self.send(msg, view=view)
+        await view.wait()
+        return view.choice
 
     async def tick(self, value=True, reaction=True):
         emojis = {True:  '<:greenTick:602811779835494410>',
@@ -115,23 +101,17 @@ class Context(commands.Context):
         """
         try:
             return await self._upload_content(content, url)
-        except:
-            try:
-                return await self._upload_content(content, 'https://hastebin.com')
-            except:
-                traceback.print_exc()
+        except Exception:
+            traceback.print_exc()
 
-    async def safe_send(self, content, file=False, filename='message_too_long.txt', url='https://mystb.in', **kwargs):
+    async def safe_send(self, content, filename='message_too_long.txt', **kwargs):
         """
         Sends to ctx.channel if possible, upload to hastebin or send text file if too long
         """
         if len(content) <= 2000:
             return await self.send(content, **kwargs)
-        elif file:
+        else:
             fp = io.BytesIO(content.encode())
             kwargs.pop('file', None)
             return await self.send(file=discord.File(fp, filename=filename), **kwargs)
-        else:
-            paste_url = await self.upload_hastebin(content, url)
-            return await self.send(f'Output too long, uploaded here instead: {paste_url}', **kwargs)
 
