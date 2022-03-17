dcd672a4cfc59004a2eafbe74f1cf084cc99b76b
Willy <19799671+Willy-C@users.noreply.github.com>
2022-03-16 22:29:36 -0400

    Redo VALORANT api stuff to use cookies instead of logging in everytime This means we do not have to bug the user for 2fa code everytime we do anything

--- cogs/valorant.py
+++ cogs/valorant.py
@@ -1,97 +1,53 @@
+import aiohttp
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from asyncpg import UniqueViolationError
 from collections import defaultdict
 
-from utils import valorantapi
+from utils.valorantapi import VALORANTAuth
 from utils.converters import CaseInsensitiveMember
 from utils.views import LoginView, _2FAView
-
+from utils.errors import MultiFactorCodeRequired
 
 
 class Valorant(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._authclients = defaultdict(dict)
-
-    def get_auth_for(self, user_id: int, username: str, password: str) -> valorantapi.VALORANTAuth:
-        if username not in self._authclients[user_id]:
-            self._authclients[user_id][username] = valorantapi.VALORANTAuth(username, password)
-
-        return self._authclients[user_id][username]
+        self._authclients: dict[int, dict[str, VALORANTAuth]] = defaultdict(dict)
+        bot.loop.create_task(self.load_cog())
 
 
-    async def fetch_login(self, user_id: int):
-        query = '''SELECT username, password, riotid 
-                   FROM valcreds 
-                   WHERE id = $1 AND private = FALSE'''
-        records = await self.bot.pool.fetch(query, user_id)
-        return records
+    def cog_unload(self) -> None:
+        for user in self._authclients.values():
+            for client in user.values():
+                self.bot.loop.create_task(client.close())
 
-    async def check_login(self, user_id: int):
-        records = await self.fetch_login(user_id)
-        if not records:
-            return None
+    @tasks.loop(hours=672) # 28 days
+    async def refresh_cookies(self):
+        for user in self._authclients.values():
+            for client in user.values():
+                await client.reauthenticate()
 
-    async def wait_for_2fa(self, ctx, auth: valorantapi.VALORANTAuth, displayname: str = None):
-        view = _2FAView(ctx=ctx)
-        name = f'for `{displayname}`' if displayname else ''
-        msg = await ctx.reply(f'Check your email and Click the button to submit 2fa code {name}', view=view)
-        await view.wait()
-        interaction = view.interaction
-        _2fa_code = getattr(view.code, 'value', None)
+    async def load_cog(self):
+        query = '''SELECT * FROM valcreds;'''
+        records = await self.bot.pool.fetch(query)
+        for r in records:
+            user_id = r['id']
+            puuid = r['puuid']
 
-        if _2fa_code is None:
-            raise ValueError('No 2fa code found')
-
-        auth._2fa_code = _2fa_code
-        await msg.delete(delay=5)
-
+            auth = VALORANTAuth.from_record(r)
+            try:
+                await auth.authenticate_from_cookies()
+            except FileNotFoundError:
+                await auth.authenticate_from_password()
+            self._authclients[user_id][puuid] = auth
+        self.refresh_cookies.start()
 
     @commands.group(name='valorant', aliases=['val'], invoke_without_command=True, case_insensitive=True)
     async def valorant_commands(self, ctx):
         await ctx.send_help(ctx.command)
 
-    @valorant_commands.command(name='shop', hidden=True, usage='')
-    async def check_shop(self, ctx, user: CaseInsensitiveMember = None):
-        user = user or ctx.author
-        logins = await self.fetch_login(user.id)
-        if not logins:
-            return await ctx.reply('I am unable to find your login!\n'
-                                   'Please make sure you saved your login with `%val login add`')
-
-        for login in logins:
-            username = login['username']
-            password = login['password']
-            display_name = login['riotid']
-
-            async with valorantapi.VALORANTAuth(username, password) as auth:
-                try:
-                    skins = await auth.check_store()
-                except valorantapi.MultiFactorCodeRequired:
-                    await self.wait_for_2fa(ctx, auth, display_name)
-                    skins = await auth.check_store()
-
-                if not skins:
-                    continue
-                names = [s['displayName'] for s in skins]
-                skin_names = "\n".join(names)
-
-                icons = [s['displayIcon'] for s in skins]
-                embeds = []
-                for icon in icons:
-                    e = discord.Embed(title='', url='https://playvalorant.com/en-us/')
-                    e.set_image(url=icon)
-                    e.set_footer(text='Click on image for full quality')
-                    embeds.append(e)
-
-                await ctx.send(f'Available skins for `{display_name}`:\n{skin_names}', embeds=embeds)
-
-    @valorant_commands.group(name='login', invoke_without_command=True, case_insensitive=True)
-    async def valorant_login_commands(self, ctx):
-        await ctx.send_help(ctx.command)
-
-    @valorant_login_commands.command(name='add')
+    @valorant_commands.command(name='login')
     async def add_account(self, ctx):
         view = LoginView(ctx=ctx)
         msg = await ctx.reply('Click the button to start the login process', view=view)
@@ -103,15 +59,17 @@ class Valorant(commands.Cog):
         if username is None or password is None:
             return await ctx.reply('Did not receive username and password')
 
-        async with valorantapi.VALORANTAuth(username, password) as auth:
+        authclient = VALORANTAuth(username=username, password=password)
+        try:
+            await authclient.authenticate_from_password()
+        except MultiFactorCodeRequired:
+            await self.wait_for_2fa(ctx, authclient)
+            await authclient.authenticate_from_2fa()
+
+        async with authclient as auth:
             try:
                 riotid = await auth.get_username()
-            except valorantapi.MultiFactorCodeRequired:
-                await self.wait_for_2fa(ctx, auth)
-                riotid = await auth.get_username()
-                # await ctx.tick(False)
-                # await interaction.followup.send('Sorry, you must 2-factor authentication disabled for me to work.')
-                # return
+                puuid = auth.puuid
             except KeyError:
                 await ctx.tick(False)
                 await interaction.followup.send('I am unable to authenticate with the given credentials.\n'
@@ -120,38 +78,62 @@ class Valorant(commands.Cog):
             finally:
                 await msg.delete(delay=20)
 
-        query = '''INSERT INTO valcreds(id, username, password, riotid)
-                   VALUES ($1, $2, $3, $4)'''
+        query = '''INSERT INTO valcreds(id, username, password, puuid, riotid)
+                   VALUES ($1, $2, $3, $4, $5)'''
         try:
-            await self.bot.pool.execute(query, ctx.author.id, username, password, riotid)
+            await self.bot.pool.execute(query, ctx.author.id, username, password, puuid, riotid)
         except UniqueViolationError:
             await ctx.tick(False)
             await interaction.followup.send(f'Error: Credentials for username `{username}` already exists!\n'
-                                            f'If you want to change your password, please use `%val login delete [username] and readd it with `%val login add``')
+                                            f'If you want to change your password, please use `%val login delete [username] and readd it with `%val login``')
         else:
             await ctx.tick()
             await interaction.followup.send(f'Added credentials for Username: {username} (Riot ID: {riotid})')
+            self._authclients[ctx.author.id][puuid] = authclient
         finally:
             await msg.delete(delay=10)
 
-    @valorant_login_commands.command(name='remove', aliases=['delete'])
-    async def delete_account(self, ctx, username):
-        query = '''DELETE FROM valcreds WHERE id = $1 AND username = $2;'''
-        status = await self.bot.pool.execute(query, ctx.author.id, username)
-        if status == 'DELETE 0':
-            await ctx.tick(False)
-            return await ctx.reply('I am unable to delete that username')
-        else:
-            await ctx.tick()
-            return await ctx.reply('Successfully deleted your credentials')
 
-    @valorant_login_commands.command(name='list')
-    async def list_accounts(self, ctx):
-        query = '''SELECT riotid FROM valcreds where id = $1;'''
-        records = await self.bot.pool.fetch(query, ctx.author.id)
-        names = [record['riotid'] for record in records]
-        fmt = ', '.join(names)
-        await ctx.reply(f'You have {len(names)} account(s) saved: {fmt}', delete_after=5)
+    async def wait_for_2fa(self, ctx, auth: VALORANTAuth, displayname: str = None):
+        view = _2FAView(ctx=ctx)
+        name = f'for `{displayname}`' if displayname else ''
+        msg = await ctx.reply(f'Seems like you have 2FA Enabled! Please check your email and Click the button to submit 2fa code {name}', view=view)
+        await view.wait()
+        # interaction = view.interaction
+        _2fa_code = getattr(view.code, 'value', None)
+
+        if _2fa_code is None:
+            raise ValueError('No 2fa code found')
+
+        auth._2fa_code = _2fa_code
+        await msg.delete(delay=5)
+
+    @valorant_commands.command(name='shop', hidden=True, usage='')
+    async def check_shop(self, ctx, user: CaseInsensitiveMember = None):
+        user = user or ctx.author
+        if user.id not in self._authclients:
+            return await ctx.reply('I am unable to find your account!\n'
+                                   'Please make sure you saved your login with `%val login`')
+
+        clients = self._authclients[user.id]
+        for puuid, client in clients.items():
+            async with client as auth:
+                skins = await auth.check_store()
+                riotid = await auth.get_username()
+                names = [s['displayName'] for s in skins]
+                skin_names = "\n".join(names)
+
+                icons = [s['displayIcon'] for s in skins]
+                embeds = []
+                for name, icon in zip(names, icons):
+                    e = discord.Embed(title=name)
+                    e.set_image(url=icon)
+                    embeds.append(e)
+
+                await ctx.send(f'Available skins for `{riotid}`:', embeds=embeds)
+
+
+
 
 
 def setup(bot):
--- utils/valorantapi.py
+++ utils/valorantapi.py
@@ -1,16 +1,19 @@
+from __future__ import annotations
 import re
 import json
 import aiohttp
 import asyncio
+import logging
 import datetime
+from pathlib import Path
 
-from typing import Tuple, List
+from typing import Tuple, List, Optional
+from utils.errors import MultiFactorCodeRequired, InvalidCredentials, Invalid2FACode, NotAuthenticated, MissingCredentials
+
+log = logging.getLogger(__name__)
 
 
-class MultiFactorCodeRequired(Exception):
-    pass
-
-
+# noinspection SpellCheckingInspection
 class VALORANTAuth:
     USER_AGENT = 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)'
 
@@ -18,33 +21,63 @@ class VALORANTAuth:
     TOKEN_URL    = 'https://entitlements.auth.riotgames.com/api/token/v1'
     USERINFO_URL = 'https://auth.riotgames.com/userinfo'
 
-    def __init__(self, username, password, *, region='na') -> None:
-        self.username: str = username
-        self.password: str = password
+    def __init__(self, *, username=None, password=None, puuid=None, riotid=None) -> None:
+        self.username: Optional[str] = username
+        self.password: Optional[str] = password
+        self.region: Optional[str] = 'na'
+        self.riotid: Optional[str] = riotid
 
-        self.entitlements_token = None
-        self.access_token = None
-        self.puuid = None
-        self.headers = {'User-Agent': self.USER_AGENT}
-        self._expire_at = None
-        self._2fa_code = None
+        self.headers: dict[str, str] = {'User-Agent': self.USER_AGENT}
+        self.entitlements_token: Optional[str] = None
+        self.access_token: Optional[str] = None
+        self.puuid: Optional[str] = puuid
+        self.id_token: Optional[str] = None
+        self._2fa_code: Optional[str] = None
 
-        self.check_valid_region(region)
-        self.region: str = region
-        self.session = None
-        self._lock = asyncio.Lock()
+        self.expires_at: Optional[datetime.datetime] = None
+        self.session: Optional[aiohttp.ClientSession] = aiohttp.ClientSession()
+        self._lock: asyncio.Lock = asyncio.Lock()
+        self._loaded_cookies = False
 
-    async def __aenter__(self):
+    @classmethod
+    def from_record(cls, data: dict):
+        return cls(username=data.get('username'),
+                   password=data.get('password'),
+                   puuid=data.get('puuid'),
+                   riotid=data.get('riotid'))
+
+    async def __aenter__(self) -> VALORANTAuth:
         await self._lock.acquire()
         if self.session is None:
             self.session = aiohttp.ClientSession()
+        await self.ensure_authenticated()
         return self
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         self._lock.release()
 
-    async def close(self):
+    async def close(self) -> None:
+        self.save_cookies()
         await self.session.close()
+
+    @property
+    def is_expired(self) -> bool:
+        if self.expires_at is None:
+            return True
+        return datetime.datetime.utcnow() >= self.expires_at
+
+    @property
+    def cookie_file(self) -> str:
+        if self.puuid is None:
+            raise MissingCredentials
+        return f'data/{self.puuid}.pickle'
+
+    def save_cookies(self):
+        self.session.cookie_jar.save(self.cookie_file)
+
+    def load_cookies(self):
+        self.session.cookie_jar.load(self.cookie_file)
+        self._loaded_cookies = True
 
     @staticmethod
     def check_valid_region(region: str) -> None:
@@ -52,33 +85,101 @@ class VALORANTAuth:
         if region not in VALID_REGIONS:
             raise ValueError(f'Invalid Region ({region}). Must be one of: {", ".join(VALID_REGIONS)}!')
 
-    # Make this a decorator maybe
+    @staticmethod
+    def parse_access_token(data) -> Tuple[str, str, int]:
+        pattern = r'access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)'
+        try:
+            uri = data['response']['parameters']['uri']
+        except KeyError:
+            raise InvalidCredentials
+        data = re.findall(pattern, uri)[0]
+        access_token = data[0]
+        id_token = data[1]
+        expires_in = int(data[2])
+        return access_token, id_token, expires_in
+
+
+    # Auth stuff
+
+
     async def ensure_authenticated(self):
-        if self._expire_at is not None and datetime.datetime.utcnow() > self._expire_at:
-            if self.access_token is not None and self.entitlements_token is not None and self.puuid is not None:
-                return
+        if self.is_expired:
+            if self._loaded_cookies:
+                try:
+                    await self.reauthenticate()
+                except InvalidCredentials:
+                    await self.authenticate_from_password()
+            else:
+                await self.authenticate_from_password()
 
-        await self.authenticate()
+    async def authenticate_from_password(self, username=None, password=None):
+        self.username = username or self.username
+        self.password = password or self.password
 
-    async def authenticate(self) -> Tuple[str, dict]:
+        if self.username is None or self.password is None:
+            raise MissingCredentials
+
         await self.prepare_cookies()
 
-        self.access_token, expire_in = await self.get_access_token()
+        self.access_token, self.id_token, expires_in = await self.get_access_token()
 
-        self._expire_at = datetime.datetime.utcnow() + datetime.timedelta(seconds=expire_in)
+        self.expires_at = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
+        self._loaded_cookies = True
 
         self.entitlements_token = await self.get_entitlement_token()
 
         self.puuid = await self.get_puuid()
 
-        self.headers = {
+        self.headers = self.final_headers()
+
+        self.save_cookies()
+        return self.puuid, self.headers
+
+    async def authenticate_from_cookies(self, puuid=None):
+        self.puuid = puuid or self.puuid
+        self.load_cookies()
+
+        self.access_token, self.id_token, expires_in = await self.reauthenticate()
+
+        self.expires_at = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
+
+        self.entitlements_token = await self.get_entitlement_token()
+
+        self.headers = self.final_headers()
+
+        self.save_cookies()
+        return self.puuid, self.headers
+
+    async def authenticate_from_2fa(self, _2fa_code=None):
+        data = await self.send_2fa_code(_2fa_code)
+        self.access_token, self.id_token, expires_in = self.parse_access_token(data)
+
+        self.expires_at = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
+        self._loaded_cookies = True
+
+        self.entitlements_token = await self.get_entitlement_token()
+
+        self.puuid = await self.get_puuid()
+
+        self.headers = self.final_headers()
+
+        self.save_cookies()
+        return self.puuid, self.headers
+
+
+    def final_headers(self):
+        if self.access_token is None or self.entitlements_token is None:
+            raise MissingCredentials
+
+        headers = {
             'Accept-Encoding': 'gzip, deflate, br',
             'User-Agent': self.USER_AGENT,
             'Authorization': f'Bearer {self.access_token}',
             'X-Riot-Entitlements-JWT':  self.entitlements_token
         }
+        return headers
 
-        return self.puuid, self.headers
+    # Auth requests
 
     async def prepare_cookies(self) -> None:
         payload = {
@@ -89,10 +190,29 @@ class VALORANTAuth:
         }
         await self.session.post(self.AUTH_URL, json=payload, headers=self.headers)
 
-    async def _2fa_access_code(self):
+    async def get_access_token(self) -> Tuple[str, str, int]:
+        payload = {
+            'type': 'auth',
+            'username': self.username,
+            'password': self.password
+        }
+        resp = await self.session.put(self.AUTH_URL,
+                                      json=payload,
+                                      headers=self.headers)
+        data = await resp.json()
+        if data['type'] == 'multifactor':
+            if self._2fa_code is None:
+                raise MultiFactorCodeRequired
+            else:
+                data = await self.send_2fa_code()
+
+        return self.parse_access_token(data)
+
+    async def send_2fa_code(self, code: str = None):
+        code = code or self._2fa_code
         payload = {
             'type': 'multifactor',
-            'code': self._2fa_code,
+            'code': code,
             "rememberDevice": True
         }
         resp = await self.session.put(self.AUTH_URL,
@@ -103,33 +223,10 @@ class VALORANTAuth:
             try:
                 data['response']['parameters']['uri']
             except KeyError:
-                print(f'Missing access_code: {data=}')
-                raise MultiFactorCodeRequired
+                log.error(f'Missing access_code: {data=}')
+                raise Invalid2FACode
             else:
                 return data
-
-    async def get_access_token(self) -> Tuple[str, int]:
-        payload = {
-            'type': 'auth',
-            'username': self.username,
-            'password': self.password
-        }
-
-        resp = await self.session.put(self.AUTH_URL,
-                                      json=payload,
-                                      headers=self.headers)
-        data = await resp.json()
-        if data['type'] == 'multifactor':
-            if self._2fa_code is None:
-                raise MultiFactorCodeRequired
-            else:
-                data = await self._2fa_access_code()
-
-        pattern = r'access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)'
-        data = re.findall(pattern, data['response']['parameters']['uri'])[0]
-        access_token = data[0]
-        expires_in = int(data[2])
-        return access_token, expires_in
 
     async def get_entitlement_token(self) -> str:
         headers = {
@@ -156,6 +253,36 @@ class VALORANTAuth:
         puuid = data['sub']
         return puuid
 
+    async def reauthenticate(self) -> Tuple[str, str, int]:
+        payload = {
+            'client_id': 'play-valorant-web-prod',
+            'nonce': '1',
+            'redirect_uri': 'https://playvalorant.com/opt_in',
+            'response_type': 'token id_token',
+        }
+        resp = await self.session.post(self.AUTH_URL, json=payload, headers=self.headers)
+        data = await resp.json()
+        return self.parse_access_token(data)
+
+
+    # API Requests
+
+
+    async def get_username(self) -> str:
+        if self.riotid:
+            return self.riotid
+
+        payload = [self.puuid]
+        resp = await self.session.put('https://pd.NA.a.pvp.net/name-service/v2/players',
+                                      headers=self.headers,
+                                      json=payload)
+        data = await resp.json(content_type=None)
+        user = data[0]
+
+        riotid = f'{user["GameName"]}#{user["TagLine"]}'
+        self.riotid = riotid
+        return riotid
+
     async def get_store_items(self) -> List[str]:
         resp = await self.session.get(f'https://pd.{self.region}.a.pvp.net/store/v2/storefront/{self.puuid}',
                                       headers=self.headers)
@@ -180,7 +307,7 @@ class VALORANTAuth:
             json.dump(skin_data, f, indent=4)
         return skin_data
 
-    async def get_current_skin_names(self, skins: List[str]) -> List[dict]:
+    async def get_current_skin_data(self, skins: List[str]) -> List[dict]:
         try:
             with open('data/skin_data.json', 'r') as f:
                 skin_data = json.load(f)
@@ -190,33 +317,5 @@ class VALORANTAuth:
         return [skin_data[skin] for skin in skins]
 
     async def check_store(self) -> List[dict]:
-        await self.ensure_authenticated()
         item_ids = await self.get_store_items()
-        return await self.get_current_skin_names(item_ids)
-
-    async def get_skin_names(self):
-        URL = 'https://valorant-api.com/v1/weapons/skins'
-        async with aiohttp.ClientSession() as session:
-            resp = await session.get(URL)
-            resp.raise_for_status()
-
-            data = await resp.json()
-
-        names = [skin['displayName'] for skin in data['data']]
-        with open('data/skin_names.json', 'w') as f:
-            json.dump(names, f, indent=4)
-
-    async def get_username(self):
-        await self.ensure_authenticated()
-        payload = [self.puuid]
-
-        resp = await self.session.put('https://pd.NA.a.pvp.net/name-service/v2/players',
-                                      headers=self.headers,
-                                      json=payload)
-        data = await resp.json(content_type=None)
-        user = data[0]
-        riotid = f'{user["GameName"]}#{user["TagLine"]}'
-
-        return riotid
-
-
+        return await self.get_current_skin_data(item_ids)
